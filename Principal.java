19b61771c94cb8ec67a5fc1728bb23484fcdd0bd
paizim <133986381+paizim@users.noreply.github.com>
2024-09-16 18:33:23 -0300

    Add files via upload

---
+++ Principal.java
@@ -0,0 +1,11 @@
+public class Principal {
+    public static void main(String[] args) {
+        Jogo jogo = new Jogo();
+        jogo.introducao();
+        jogo.reino1();
+        jogo.deckPersonagem1();
+        jogo.reino2();
+        jogo.deckPersonagem2();
+        jogo.iniciarBatalha();
+    }
+}
