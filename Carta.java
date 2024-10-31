6871997300d022eddef056095f94c1acd344d8f4
Ladstone <ladssilva@gmail.com>
2024-10-31 02:01:04 -0300

    Update Carta.java

--- Carta.java
+++ Carta.java
@@ -1,12 +1,21 @@
 public class Carta {
-    String nome;
-    int custoMana;
+    protected String nome;
+    protected int custoMana;
 
     public Carta(String nome, int custoMana) {
         this.nome = nome;
         this.custoMana = custoMana;
     }
 
+    public String getNome() {
+        return nome;
+    }
+
+    public int getCustoMana() {
+        return custoMana;
+    }
+
+    @Override
     public String toString() {
         return "Nome: " + nome + ", Custo de Mana: " + custoMana;
     }
